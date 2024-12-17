ea145184910403e3085f37a0dd2fbe25893454a9
Mohamed Mustafa <pmohmmedmustafa@gmail.com>
2024-12-18 00:31:08 +0200

    feat: update test script

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 python3 -m test \
-        --data_path ../aug_pre/ \
+        --data_path ../aug_pre/test_data \
         --results_path ./results/\
         --model_path pre_trained/unet.keras \
         --pre_obj_path data/pre.pkl \
